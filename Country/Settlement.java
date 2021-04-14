5c01139e5a4af2782ff999a5d5f3be6948bf23d4
arielga1 <80514359+arielga1@users.noreply.github.com>
2021-04-14 11:41:16 +0300

    Add files via upload

--- Country/Settlement.java
+++ Country/Settlement.java
@@ -22,7 +22,7 @@ public abstract class Settlement {
      * @param people        The people living in the settlement.
      */
     public Settlement(String name, Location location,
-                      List<Person> people) {
+                      List<Person> people, RamzorColor ramzorColor) {
                         this.name = name;
                         this.location = location;
                         if (people == null) {
@@ -36,8 +36,56 @@ public abstract class Settlement {
      * Calculates the ramzor grade of the city.
      * @return              The grade.
      */
+	public String get_Name() {
+		return name;
+	}
+	public Location get_Location() {
+		return location;
+	}
+	
+	public List<Person> get_People() {
+		return people;
+	}
+	public RamzorColor get_RamzorColor() {
+		return ramzorColor;
+	}
+	
+	public void set_People(List<Person> p) {
+		this.people.clear();
+		for(int i=0;i<p.size();++i)
+			this.people.add(i, p.get(i));
+	}
+	public void set_Name(String name) {
+		this.name = name;
+	}
+	
+	public void set_Location(Location location) {
+		this.location = location;
+	}
+	
+	public void set_RamzorColor(RamzorColor ramzorColor) {
+		this.ramzorColor = ramzorColor;
+	}
+	
+	public abstract RamzorColor calculateRamzorGrade();
+	
+	
+	public double contagiousPercent() { 
+		int count=0;
+		for (int i=0;i<people.size();++i)
+		{
+			if (people.get(i) instanceof Sick)
+				++count;
+		}
+		return count/people.size();
+	}
+	
+	public Point randomLocation()
+	{
+		return new Point((int)Math.random(),(int)Math.random());
+	}
+	
     public abstract RamzorColor calculateRamzorGrade();
-
     @Override
     public boolean equals(Object other) {
         if (!(other instanceof Settlement))
@@ -48,4 +96,18 @@ public abstract class Settlement {
                 this.people.equals(set.people) &&
                 this.ramzorColor.equals(set.ramzorColor);
     }
+    
+	public boolean transferPerson(Person person, Settlement settlement) {
+		return true;
+	}
+	
+	public boolean addPerson(Person p) {
+		Person [] new_people= new Person[people.length+1];
+		for(int i=0;i<new_people.length;i++)
+		{
+			new_people[i]=this.people[i];
+		}
+		new_people[new_people.length-1]=p;
+		return true;
+	}
 }
