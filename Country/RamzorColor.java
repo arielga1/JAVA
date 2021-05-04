9a23c282ba902df58f8b3f5a79a3c848c9543f37
Ariel <Ariel@Ariel-PC>
2021-05-04 21:50:08 +0300

    names and ids added

--- Country/RamzorColor.java
+++ Country/RamzorColor.java
@@ -8,16 +8,17 @@ package Country;
  *
  */
 public enum RamzorColor {
-    Green(0.4), Yellow(0.6), Orange(0.8), Red(1.0);
-
+    GREEN(0.4, 1.0, Color.GREEN),
+    YELLOW(0.6, 0.8, Color.YELLOW),
+    ORANGE(0.8, 0.6, Color.ORANGE),
+    RED(1.0, 0.4, Color.RED);
     private double value;
 
     /**
      * Constructor.
      * @param v             The value of the coefficient.
      */
-    RamzorColor(double v) { this.value = v; }
-
+   RamzorColor(double v), double vt, Color color){ this.value = v; }
     /**
      * Retrieves the coefficient of the color.
      * @return              The coefficient.
@@ -30,17 +31,15 @@ public enum RamzorColor {
      * @return              The grade for the value.
      */
     public static RamzorColor getGrade(double value) {
-        if (value <= Green.value)
-            return Green;
-        else if (value <= Yellow.value)
-            return Yellow;
-        else if (value <= Orange.value)
-            return Orange;
-        else
-            return Red;
+        if (v > 0 && v <= 0.4) return GREEN;
+        if (v > 0.4 && v <= 0.6) return YELLOW;
+        if (v > 0.6 && v <= 0.8) return ORANGE;
+        if (v > 0.8 && v <= 1.0) return RED;
+        return GREEN;
     }
 
     public static void main(String [] args) {
         System.out.println(RamzorColor.Green.coefficient());
     }
 }
+
